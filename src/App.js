ab3196f9b66728874347412c0492837df575c253
IvanOpushkin <farm.vanya@yandex.ru>
2021-07-05 21:01:36 +1000

    complex staff done finishing + analyse next hour try to understand

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
@@ -23,15 +23,19 @@ const firestore = firebase.firestore();
 
 
 function App() {
+
+    const [user] = useAuthState(auth);
+    
   return (
     <div className="App">
-      <header className="App-header">
-       Hello
-
-        {SignIn()}
-       {ChatRoom()}
-
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
@@ -40,17 +44,20 @@ function SignIn(){
     const signInWithGoogle = () =>
     {
         const provider = new firebase.auth.GoogleAuthProvider();
-        auth.singInWithPopup(provider);
+        auth.signInWithPopup(provider);
     }
 
-    return <button onClick={signInWithGoogle}>Sign in with Google</button>
-}
+    return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )}
 
-function SignOut(){
-    return auth.currentUser && (
-
-        <button onClick={() => auth.signOut}>Sign Out</button>
-    )
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 function ChatRoom(){
@@ -58,16 +65,39 @@ function ChatRoom(){
     const query = messagesRef.orderBy('createdAt').limit(25);
 
     const [messages] = useCollectionData(query, {idField: 'id'});
+    const [formValue, setFormValue] = useState('');
+
+    const sendMessage = async(e) =>
+    {
+        e.preventDefault();
+
+        const {uid, photoURL} = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+          });
+
+          setFormValue('');
+    }
 
 
-    return (
-        <>
-            <div>
-                {messages && messages.map(msg => <ChatMessage key = {msg.id} message = {msg}/>)}
-            </div>
+    return (<>
+        <main>
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        </main>
             
-        </>
-   )
+        <form onSubmit={sendMessage}>
+
+         <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+         <button type="submit" disabled={!formValue}>🕊️</button>
+
+        </form>
+
+    </>)
 
 
 }
@@ -81,7 +111,7 @@ function ChatMessage(props){
     <img src="{photoURL}"/>
     <p>{text}</p>
     </div>) 
-    
+
 }
 
 export default App;
